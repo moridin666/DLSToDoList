54b307af6a59409e90a448925f0f4b6b754c48db
Jason Schneider <j.a.schneidr@gmail.com>
2014-04-29 14:17:30 -0400

    Initial Commit

---
+++ DLSToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DLSToDoList
+//
+//  Created by Jason Schneider on 4/29/14.
+//  Copyright (c) 2014 Jason Schneider. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DLSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DLSAppDelegate class]));
+    }
+}
