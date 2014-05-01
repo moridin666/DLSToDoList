985c570d01be9bc37bbb9354bf52bc2be79a33c1
Jason Schneider <j.a.schneidr@gmail.com>
2014-05-01 17:41:56 -0400

    updated file headers

--- DLSToDoList/DLSToDoItem.m
+++ DLSToDoList/DLSToDoItem.m
@@ -2,7 +2,7 @@
 //  DLSToDoItem.m
 //  DLSToDoList
 //
-//  Created by Jason Schneider on 4/29/14.
+//  Created by Jason Schneider on 4/29/14 in the name of Dark Lord Software.
 //  Copyright (c) 2014 Jason Schneider. All rights reserved.
 //
 
